91e2b7ad0cd02309f3f7247c76251c167c4e50c4
yinyazuolun <yinyazuolun@sina.com>
2015-04-17 12:01:57 +0800

    添加测试项目

---
+++ src/Carving.Forest/Carving.Test/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Carving.Application;
+using Carving.Domain.Model;
+using Carving.Domain.Repository.EF.Data;
+using Carving.Infrastructrue.Autofac;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Xunit;
+
+namespace Carving.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            var tableRepository = new Mock<ITableRepository>();
+            tableRepository.Setup(x => x.FindAll()).Returns(new List<Table>());
+            var a = tableRepository.Object.FindAll();
+
+            var scanCodeServices = ServiceLocator.Instance.GetService<IScanCodeServices>();
+            scanCodeServices.ScanCode();
+        }
+    }
+}
