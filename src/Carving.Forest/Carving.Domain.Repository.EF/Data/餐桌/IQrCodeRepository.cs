ada94e6bd8cba180bf3b2e1104ddd758981ba4ed
yinyazuolun <yinyazuolun@sina.com>
2015-05-04 15:17:58 +0800

    添加遗漏文件

---
+++ src/Carving.Forest/Carving.Domain.Repository.EF/Data/餐桌/IQrCodeRepository.cs
@@ -0,0 +1,15 @@
+﻿// Carving.Domain.Repository.EF IQrCodeRepository.cs
+// writer sundy
+// Last Update Time 2015-04-29-20:22
+// Create Time 2015-04-29-20:22
+
+using Carving.Domain.Core.Repositories;
+using Carving.Domain.Model;
+
+namespace Carving.Domain.Repository.EF.Data
+{
+    public interface IQrCodeRepository : IRepository<QrCode>
+    {
+
+    }
+}
