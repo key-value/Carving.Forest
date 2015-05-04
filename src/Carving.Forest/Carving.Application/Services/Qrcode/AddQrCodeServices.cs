05b4729abf0da73c107e83e460b52b458c9f9723
yinyazuolun <yinyazuolun@sina.com>
2015-05-04 14:44:16 +0800

    添加遗漏文件

---
+++ src/Carving.Forest/Carving.Application/Services/Qrcode/AddQrCodeServices.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using Carving.Domain.Model;
+using Carving.Domain.Repository.EF.Data;
+using Carving.Infrastructrue.Aop;
+
+namespace Carving.Application.Services.Qrcode
+{
+    [Injection(typeof(IAddQrCodeServices))]
+    public class AddQrCodeServices : ApplicationServices, IAddQrCodeServices
+    {
+        public IQrCodeRepository QrCodeRepository { private get; set; }
+
+
+        public int Create(string code, Guid tableID)
+        {
+            var qrCode = new QrCode();
+            qrCode.ID = Guid.NewGuid();
+            qrCode.Code = code;
+            qrCode.TableID = tableID;
+            QrCodeRepository.Add(qrCode);
+            RepositoryContext.Commit();
+            return 1;
+        }
+    }
+}
---
+++ src/Carving.Forest/Carving.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Carving.Application;
+using Carving.Infrastructrue.Autofac;
+
+namespace Carving.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var scanCodeServices = ServiceLocator.Instance.GetService<IScanCodeServices>();
+            scanCodeServices.ScanCode();
+            return View();
+        }
+
+        //
+        // GET: /Home/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Home/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Home/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Home/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
